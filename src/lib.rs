d0bfb6a437c68fb9b0bb54e6bb8b66261ccf2a60
Ronald Kinard <furyhunter600@gmail.com>
2015-09-17 23:10:58 -0500

    Add Sdmc struct.
    This won't be useful until we have libstd, but worth making since
    it's trivial.

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,13 @@ pub type Handle = u32;
 
 pub mod srv;
 pub mod gfx;
+pub mod sdmc;
+
 pub mod services;
 
 pub use srv::Srv;
 pub use gfx::Gfx;
+pub use sdmc::Sdmc;
 
 #[lang = "stack_exhausted"] extern fn stack_exhausted() {}
 #[lang = "eh_personality"] extern fn eh_personality() {}
---
+++ src/sdmc.rs
@@ -0,0 +1,28 @@
+use core::marker::PhantomData;
+
+use ::raw::sdmc::*;
+
+pub struct Sdmc {
+    pd: PhantomData<i32>
+}
+
+impl Sdmc {
+    pub fn new() -> Result<Self, i32> {
+        unsafe {
+            let r = sdmcInit();
+            if r < 0 {
+                Err(r)
+            } else {
+                Ok(Sdmc { pd: PhantomData })
+            }
+        }
+    }
+}
+
+impl Drop for Sdmc {
+    fn drop(&mut self) {
+        unsafe {
+            sdmcExit()
+        };
+    }
+}
