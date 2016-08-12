0c8a465e930f3aabfda39540e9eae22425c1cfe2
Fenrir <fenrirwolf@gmail.com>
2016-08-12 15:59:22 -0700

    Add Path and OsString APIs

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,35 @@
-#![feature(alloc, collections, lang_items)]
+#![feature(alloc)]
+#![feature(collections)]
+#![feature(char_escape_debug)]
+#![feature(lang_items)]
+#![feature(question_mark)]
+#![feature(slice_patterns)]
+#![feature(str_internals)]
+#![feature(unicode)]
+
 #![no_std]
+
 #![crate_type = "rlib"]
 #![crate_name = "ctru"]
 
-extern crate ctru_sys as libctru;
-
 extern crate alloc;
 extern crate collections;
+extern crate rustc_unicode;
+
+extern crate ctru_sys as libctru;
 
 pub mod console;
 pub mod srv;
 pub mod gfx;
+pub mod services;
 pub mod sdmc;
 
-pub mod services;
+pub mod ascii;
+pub mod ffi;
 pub mod panic;
+pub mod path;
+
+mod sys;
 
 pub use srv::Srv;
 pub use gfx::Gfx;
