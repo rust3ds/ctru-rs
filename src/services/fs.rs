9c2c664b63b522df408389b6d85190081143cda2
Fenrir <fenrirwolf@gmail.com>
2016-08-13 23:36:27 -0700

    Add OpenOptions

--- src/services/fs.rs
+++ src/services/fs.rs
@@ -4,95 +4,7 @@ use collections::Vec;
 
 use path::Path;
 
-use libctru::services::fs;
-
-pub struct Fs {
-    pd: PhantomData<i32>,
-}
-
-impl Fs {
-    pub fn init() -> Result<Fs, i32> {
-        unsafe {
-            let r = fs::fsInit();
-            if r < 0 {
-                Err(r)
-            } else {
-                Ok(Fs { pd: PhantomData })
-            }
-        }
-    }
-
-    pub fn sdmc(&self) -> Result<Archive, i32> {
-        let mut handle = 0u64;
-        unsafe {
-            let id = ArchiveID::Sdmc;
-            let path = fs::fsMakePath(PathType::Empty.into(), ptr::null() as *const _);
-            let ret = fs::FSUSER_OpenArchive(&mut handle, id.into(), path);
-            if ret < 0 {
-                Err(ret)
-            } else {
-                Ok(Archive {
-                    handle: handle,
-                    id: id,
-                })
-            }
-        }
-    }
-}
-
-impl Drop for Fs {
-    fn drop(&mut self) {
-        unsafe {
-            fs::fsExit();
-        }
-    }
-}
-
-
-pub struct Archive {
-    id: ArchiveID,
-    handle: u64,
-}
-
-impl Archive {
-    pub fn open_file(&self, p: &Path) -> Result<File, i32> {
-        unsafe {
-            let mut handle: u32 = 0;
-            let wide = p.as_os_str().encode_wide().collect::<Vec<_>>();
-            let path = fs::fsMakePath(PathType::UTF16.into(), wide.as_slice().as_ptr() as *mut _);
-            let ret = fs::FSUSER_OpenFile(&mut handle, self.handle, path, fs::FS_OPEN_READ, 0);
-            if ret < 0 {
-                Err(ret)
-            } else {
-                Ok(File { handle: handle })
-            }
-        }
-    }
-
-    pub fn id(&self) -> ArchiveID {
-        self.id
-    }
-}
-
-impl Drop for Archive {
-    fn drop(&mut self) {
-        unsafe {
-            fs::FSUSER_CloseArchive(self.handle);
-        }
-    }
-}
-
-pub struct File {
-    handle: u32,
-}
-
-impl Drop for File {
-    fn drop(&mut self) {
-        unsafe {
-            fs::FSFILE_Close(self.handle);
-        }
-    }
-}
+use libctru::services::fs::*;
 
 #[derive(Copy, Clone, Debug)]
 pub enum PathType {
@@ -101,34 +13,6 @@ pub enum PathType {
     Binary,
     ASCII,
     UTF16,
-}
-
-impl From<PathType> for fs::FS_PathType {
-    fn from(p: PathType) -> Self {
-        use self::PathType::*;
-        use libctru::services::fs::FS_PathType::*;
-        match p {
-            Invalid => PATH_INVALID,
-            Empty => PATH_EMPTY,
-            Binary => PATH_BINARY,
-            ASCII => PATH_ASCII,
-            UTF16 => PATH_UTF16,
-        }
-    }
-}
-
-impl From<fs::FS_PathType> for PathType {
-    fn from(f: fs::FS_PathType) -> Self {
-        use self::PathType::*;
-        use libctru::services::fs::FS_PathType::*;
-        match f {
-            PATH_INVALID => Invalid,
-            PATH_EMPTY => Empty,
-            PATH_BINARY => Binary,
-            PATH_ASCII => ASCII,
-            PATH_UTF16 => UTF16,
-        }
-    }
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -157,7 +41,176 @@ pub enum ArchiveID {
     DemoSavedata,
 }
 
-impl From<ArchiveID> for fs::FS_ArchiveID {
+
+pub struct Fs {
+    pd: PhantomData<i32>,
+}
+
+pub struct Archive {
+    id: ArchiveID,
+    handle: u64,
+}
+
+pub struct File {
+    handle: u32,
+}
+
+pub struct OpenOptions {
+    read: bool,
+    write: bool,
+    create: bool,
+    arch_handle: u64,
+}
+
+impl Fs {
+    pub fn init() -> Result<Fs, i32> {
+        unsafe {
+            let r = fsInit();
+            if r < 0 {
+                Err(r)
+            } else {
+                Ok(Fs { pd: PhantomData })
+            }
+        }
+    }
+
+    pub fn sdmc(&self) -> Result<Archive, i32> {
+        let mut handle = 0u64;
+        unsafe {
+            let id = ArchiveID::Sdmc;
+            let path = fsMakePath(PathType::Empty.into(), ptr::null() as *const _);
+            let ret = FSUSER_OpenArchive(&mut handle, id.into(), path);
+            if ret < 0 {
+                Err(ret)
+            } else {
+                Ok(Archive {
+                    handle: handle,
+                    id: id,
+                })
+            }
+        }
+    }
+}
+
+impl Archive {
+    pub fn file_open(&self, path: &Path) -> Result<File, i32> {
+        self.file_open_options().read(true).create(true).open(path)
+    }
+
+    pub fn file_open_options(&self) -> OpenOptions {
+        OpenOptions {
+            read: false,
+            write: false,
+            create: false,
+            arch_handle: self.handle,
+        }
+    }
+
+    pub fn id(&self) -> ArchiveID {
+        self.id
+    }
+}
+
+impl OpenOptions {
+    pub fn read(&mut self, read: bool) -> &mut OpenOptions {
+        self.read = read;
+        self
+    }
+
+    pub fn write(&mut self, write: bool) -> &mut OpenOptions {
+        self.write = write;
+        self
+    }
+
+    pub fn create(&mut self, create: bool) -> &mut OpenOptions {
+        self.create = create;
+        self
+    }
+
+    pub fn open<P: AsRef<Path>>(&self, path: P) -> Result<File, i32> {
+        self._open(path.as_ref(), self.get_open_flags())
+    }
+
+    fn _open(&self, path: &Path, flags: u32) -> Result<File, i32> {
+        unsafe {
+            let mut file_handle: u32 = 0;
+            let wide = path.as_os_str().encode_wide().collect::<Vec<_>>();
+            let ctr_path = fsMakePath(PathType::UTF16.into(), wide.as_slice().as_ptr() as *mut _);
+            let ret = FSUSER_OpenFile(&mut file_handle, self.arch_handle, ctr_path, flags, 0);
+            if ret < 0 {
+                Err(ret)
+            } else {
+                Ok(File { handle: file_handle })
+            }
+        }
+    }
+
+    fn get_open_flags(&self) -> u32 {
+        match (self.read, self.write, self.create) {
+            (true,  false, false) => FS_OPEN_READ,
+            (false, true,  false) => FS_OPEN_WRITE,
+            (true,  false, true)  => FS_OPEN_READ | FS_OPEN_CREATE,
+            (true,  true,  false) => FS_OPEN_READ | FS_OPEN_WRITE,
+            (true,  true,  true)  => FS_OPEN_READ | FS_OPEN_WRITE | FS_OPEN_CREATE,
+            _ => 0, //failure case
+        }
+    }
+}
+
+impl Drop for Fs {
+    fn drop(&mut self) {
+        unsafe {
+            fsExit();
+        }
+    }
+}
+
+
+impl Drop for Archive {
+    fn drop(&mut self) {
+        unsafe {
+            FSUSER_CloseArchive(self.handle);
+        }
+    }
+}
+
+impl Drop for File {
+    fn drop(&mut self) {
+        unsafe {
+            FSFILE_Close(self.handle);
+        }
+    }
+}
+
+impl From<PathType> for FS_PathType {
+    fn from(p: PathType) -> Self {
+        use self::PathType::*;
+        use libctru::services::fs::FS_PathType::*;
+        match p {
+            Invalid => PATH_INVALID,
+            Empty => PATH_EMPTY,
+            Binary => PATH_BINARY,
+            ASCII => PATH_ASCII,
+            UTF16 => PATH_UTF16,
+        }
+    }
+}
+
+impl From<FS_PathType> for PathType {
+    fn from(f: FS_PathType) -> Self {
+        use self::PathType::*;
+        use libctru::services::fs::FS_PathType::*;
+        match f {
+            PATH_INVALID => Invalid,
+            PATH_EMPTY => Empty,
+            PATH_BINARY => Binary,
+            PATH_ASCII => ASCII,
+            PATH_UTF16 => UTF16,
+        }
+    }
+}
+
+impl From<ArchiveID> for FS_ArchiveID {
     fn from(a: ArchiveID) -> Self {
         use self::ArchiveID::*;
         use libctru::services::fs::FS_ArchiveID::*;
