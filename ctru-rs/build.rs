1fc8156d31aac83a945cd21c436a745e319b2ba6
Fenrir <fenrirwolf@gmail.com>
2017-07-07 17:33:02 -0600

    Fix ctru-rs build script
    The debug/release stuff isn't used until the next official release of libctru

--- ctru-rs/build.rs
+++ ctru-rs/build.rs
@@ -3,11 +3,5 @@ use std::path::PathBuf;
 
 fn main() {
     let dkp_path = PathBuf::from(env::var("DEVKITPRO").unwrap());
-
     println!("cargo:rustc-link-search=native={}", dkp_path.join("libctru/lib").display());
-    println!("cargo:rustc-link-lib=static={}", match env::var("PROFILE").unwrap().as_str() {
-        "release" => "ctru",
-        "debug" => "ctrud",
-        _ => unreachable!(),
-    });
 }
