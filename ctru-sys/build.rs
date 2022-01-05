f776cc18c628ccfa5b1524e4fcaca4b4ea0217a4
AzureMarker <mark.drobnak@gmail.com>
2022-01-04 17:12:49 -0800

    Minimize ctru-sys build script churn

--- ctru-sys/build.rs
+++ ctru-sys/build.rs
@@ -4,6 +4,8 @@ fn main() {
     let dkp_path = env::var("DEVKITPRO").unwrap();
     let profile = env::var("PROFILE").unwrap();
 
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-env-changed=DEVKITPRO");
     println!("cargo:rustc-link-search=native={}/libctru/lib", dkp_path);
     println!(
         "cargo:rustc-link-lib=static={}",
