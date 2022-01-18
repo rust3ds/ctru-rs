6b5d420ed2e9db495ac4791a1547805b427ce4db
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2022-01-18 09:37:01 -0500

    Use a module instead of include!
    Modules are better understood by rust-analyzer compared to macros
    expansion, and we can still use the same bindgen script and lint attrs.

--- ctru-sys/src/lib.rs
+++ ctru-sys/src/lib.rs
@@ -1,7 +1,9 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(clippy::all)]
 #![no_std]
 
-include!("bindings.rs");
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+#[allow(non_snake_case)]
+#[allow(clippy::all)]
+mod bindings;
+
+pub use bindings::*;
