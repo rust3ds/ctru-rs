c59de27f853dc69bb8087cad074f06bba591d444
Andrea Ciliberti <meziu210@icloud.com>
2022-02-01 18:37:17 +0100

    Removed link field in ctru-sys

--- ctru-rs/examples/hello-world.rs
+++ ctru-rs/examples/hello-world.rs
@@ -12,16 +12,22 @@ fn main() {
     let apt = Apt::init().expect("Couldn't obtain APT controller");
     let _console = Console::init(gfx.top_screen.borrow_mut());
 
-    let out = b"Hello fellow Rustaceans, I'm on the Nintendo 3DS!";
-    let width = 24;
+    struct Timespec {
+        t: libc::timespec,
+    }
 
-    let mut writer = BufWriter::new(Vec::new());
-    ferris_says::say(out, width, &mut writer).unwrap();
-
+    //let inst = std::time::Instant::now();
+    let mut t = Timespec { t: libc::timespec { tv_sec: 0, tv_nsec: 0 } };
+    let res = unsafe { libc::clock_gettime(libc::CLOCK_MONOTONIC, &mut t.t) };
+    println!("{{tv_sec: {}, tv_nsec: {} }} {} clock:{}", t.t.tv_sec, t.t.tv_nsec, res, libc::CLOCK_MONOTONIC);
+    ctru::thread::sleep(std::time::Duration::from_secs(2));
+    /*let ela = inst.elapsed();
     println!(
-        "\x1b[0;0H{}",
-        String::from_utf8_lossy(&writer.into_inner().unwrap())
-    );
+        "\x1b[0;0HElapsed: {:#?}",
+        ela
+    );*/
+
+
 
     // Main loop
     while apt.main_loop() {
