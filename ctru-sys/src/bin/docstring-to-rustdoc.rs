99c7a36ef056bc26fccf9f5578a402c14e88f605
TechiePi <techie@techiepi.dev>
2022-10-10 22:35:12 +0200

    Update ``doxygen-rs``

--- ctru-sys/src/bin/docstring-to-rustdoc.rs
+++ ctru-sys/src/bin/docstring-to-rustdoc.rs
@@ -16,8 +16,9 @@ fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     let bindings_path = Path::new(args.get(1).expect("bindings.rs not provided in the args"));
+    let bindings = fs::read_to_string(bindings_path)?;
 
-    let parsed = doxygen_rs::transform_bindgen(&bindings_path)?;
+    let parsed = doxygen_rs::transform_bindgen(bindings.as_str());
 
     let old_bindings_path = bindings_path.to_str().unwrap().to_owned() + ".old";
 
