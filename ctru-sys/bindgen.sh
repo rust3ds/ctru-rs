025d60356daec5334a2871df012c5db8fa17c441
TechiePi <techie@techiepi.dev>
2023-04-04 18:49:41 +0200

    Add `bindgen-ctru-sys` package and update bindings
    - Added the `bindgen-ctru-sys` to be able to use `ParseCallbacks` when generating the bindings.
    - Updated `doxygen-rs` to 0.4, which has a faster engine and more extensible internal (even though there are some regressions).
    - Removes the `docstring-to-rustdoc` package because it is no longer needed.

--- ctru-sys/bindgen.sh
+++ ctru-sys/bindgen.sh
@@ -21,35 +21,7 @@ CTRU_SYS_VERSION="$(
 )"
 
 echo "Generating bindings.rs..."
-bindgen "$DEVKITPRO/libctru/include/3ds.h" \
-    --rust-target nightly \
-    --use-core \
-    --distrust-clang-mangling \
-    --must-use-type 'Result' \
-    --no-layout-tests \
-    --ctypes-prefix "::libc" \
-    --no-prepend-enum-name \
-    --generate "functions,types,vars" \
-    --blocklist-type "u(8|16|32|64)" \
-    --blocklist-type "__builtin_va_list" \
-    --blocklist-type "__va_list" \
-    --opaque-type "MiiData" \
-    --with-derive-default \
-    -- \
-    --target=arm-none-eabi \
-    --sysroot="$DEVKITARM/arm-none-eabi" \
-    -isystem"$DEVKITARM/arm-none-eabi/include" \
-    -I"$DEVKITPRO/libctru/include" \
-    -mfloat-abi=hard \
-    -march=armv6k \
-    -mtune=mpcore \
-    -mfpu=vfp \
-    -DARM11 \
-    -D__3DS__ \
-> src/bindings.rs
-
-echo "Updating docstrings in bindings.rs..."
-cargo run --quiet --package docstring-to-rustdoc -- src/bindings.rs
+cargo run --package bindgen-ctru-sys > src/bindings.rs
 
 echo "Formatting generated files..."
 cargo fmt --all
