8334221f521b76a34abc3c896ac0e95eee8f2d3d
Andrea Ciliberti <meziu210@icloud.com>
2023-04-08 14:17:08 +0200

    Compilation error when romfs can't be found

--- ctru-rs/src/services/mod.rs
+++ ctru-rs/src/services/mod.rs
@@ -34,6 +34,8 @@ cfg_if::cfg_if! {
             //! [package.metadata.cargo-3ds]
             //! romfs_dir = "romfs"
             //! ```
+            
+            compile_error!("romfs feature is enabled but no romfs found!");
         }
     }
 }
