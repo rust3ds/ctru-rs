f2cb18e26a184bdc7fb8aad85828e9e87985764b
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2024-04-22 22:51:39 -0400

    Initial pass of generated C layout tests

---
+++ ctru-sys/tests/layout_test.rs
@@ -0,0 +1,18 @@
+//! This is a stub for the generated layout test. We use bindgen callbacks along
+//! with the [`cpp`] crate to compile actual `sizeof` and `alignof` calls in C,
+//! as opposed to bindgen's generated layout tests which use hardcoded size literals.
+//!
+//! This should help ensure that the generated bindings are correct for the actual
+//! ABI used by libctru and the devkitARM toolchain, instead of just what libclang
+//! thinks they should be at bindgen time.
+
+#![allow(non_snake_case)]
+#![feature(custom_test_frameworks)]
+#![test_runner(test_runner::run_gdb)]
+
+extern crate shim_3ds;
+
+// TODO: might want to move this into a test crate so we can avoid compiling it
+// for non-test builds? Idk if there's a reasonable way to do it though.
+
+include!(concat!(env!("OUT_DIR"), "/layout_test.rs"));
