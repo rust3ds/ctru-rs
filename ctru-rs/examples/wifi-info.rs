b7d6cc4ef72b606500108c28396fc4db6c24a99c
Andrea Ciliberti <meziu210@icloud.com>
2025-08-09 11:55:50 +0200

    Update example wifi-info

--- ctru-rs/examples/wifi-info.rs
+++ ctru-rs/examples/wifi-info.rs
@@ -2,16 +2,22 @@
 //!
 //! This example prints out all the info about the console's network, like SSID, security, proxy info...
 
-use ctru::{prelude::*, services::ac::Ac};
+use ctru::{
+    prelude::*,
+    services::ac::{Ac, NetworkStatus},
+};
+use std::error::Error;
 
 fn main() {
     let gfx = Gfx::new().expect("Couldn't obtain GFX controller");
     let mut hid = Hid::new().expect("Couldn't obtain HID controller");
     let apt = Apt::new().expect("Couldn't obtain APT controller");
 
+    ctru::set_panic_hook(true);
+
     let _console = Console::new(gfx.top_screen.borrow_mut());
 
-    let mut ac = Ac::new().expect("Couldn't get an AC handle");
+    let ac = Ac::new().expect("Couldn't get an AC handle");
 
     print_network_info(&ac).expect("Error while gathering network info");
     println!("Press START to exit.");
@@ -27,21 +33,32 @@ fn main() {
     }
 }
 
-fn print_network_info(ac: &Ac) -> ctru::Result<()> {
-    let connected = ac.get_wifi_status()?;
-    println!("Wi-Fi status: {:?}", connected);
+fn print_network_info(ac: &Ac) -> Result<(), Box<dyn Error>> {
+    let status = ac.wifi_status()?;
+    println!("Wi-Fi status: {:?}", status);
 
     // Some methods error out if the console isn't connected
-    if connected {
-        println!("Wi-Fi SSID: {}", ac.get_wifi_ssid()?);
-        println!("Wi-Fi security: {:?}", ac.get_wifi_security()?);
-        let proxied = ac.get_proxy_enabled()?;
+    if matches!(
+        status,
+        NetworkStatus::WANConnected | NetworkStatus::LANConnected
+    ) {
+        println!("Wi-Fi SSID: {}", String::from_utf8(ac.wifi_ssid()?)?);
+        println!("Wi-Fi security: {:?}", ac.wifi_security()?);
+        let proxied = ac.proxy_enabled()?;
         println!("Proxy enabled: {}", proxied);
         if proxied {
-            println!("Proxy port: {}", ac.get_proxy_port()?);
-            println!("Proxy username: {}", ac.get_proxy_username()?);
-            println!("Proxy password: {}", ac.get_proxy_password()?);
+            println!("Proxy port: {}", ac.proxy_port()?);
+            println!(
+                "Proxy username: {}",
+                String::from_utf8(ac.proxy_username()?)?
+            );
+            println!(
+                "Proxy password: {}",
+                String::from_utf8(ac.proxy_password()?)?
+            );
         }
+    } else {
+        println!("Not connected to any network.")
     }
 
     Ok(())
